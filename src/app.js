2990066298760fd12f5a1f5eef704ec5c28cc29b
Hamish-H <hamish.h@outlook.com>
2018-06-25 23:08:31 +0100

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const InputView = require('./views/input_view.js');
-const OutputView = require('./views/output_view.js');
+const OutPutView = require('./views/output_view.js');
 const WordCounter = require('./models/word_counter.js');
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -7,5 +7,11 @@ console.log('js loaded');
 
 const inputView = new InputView();
   inputView.bindEvents();
+
+const wordCounter = new WordCounter();
+wordCounter.bindEvents();
+
+const outPutView = new OutputView();
+  outPutView.bindEvents();
 
 });
