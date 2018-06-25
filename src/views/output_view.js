6b1844905b1244ef915a516d86b69f3fd7c6a327
Hamish-H <hamish.h@outlook.com>
2018-06-25 22:51:40 +0100

    updated views

--- src/views/output_view.js
+++ src/views/output_view.js
@@ -1,14 +1,7 @@
 const PubSub = require('../helpers/pub_sub.js');
-
 const OutputView = function () {
-
 };
 
-InputView.prototype.bindEvents = function () {
-  const input = document.querySelector('#text');
-  input.addEventListener('input',(event) =>{
-  const inputtedNumber = event.target.value;
-};
 
 
 
