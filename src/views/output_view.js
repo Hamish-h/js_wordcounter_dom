bbb41577a63e65ad46b8eb298011390b3d043bda
Hamish-H <hamish.h@outlook.com>
2018-06-25 22:53:46 +0100

    updated output view.js

--- src/views/output_view.js
+++ src/views/output_view.js
@@ -2,7 +2,11 @@ const PubSub = require('../helpers/pub_sub.js');
 const OutputView = function () {
 };
 
-
-
+ResultView.prototype.bindEvents = function () {
+  PubSub.subscribe('PrimeChecker:result', (event) => {
+    const result = event.detail;
+    this.updateView(result);
+  })
+};
 
 module.exports = OutputView;
