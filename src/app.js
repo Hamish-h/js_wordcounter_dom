d55def5890c8591df52eaee57b52f0922a172118
Hamish-H <hamish.h@outlook.com>
2018-06-25 23:06:05 +0100

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {}
+const InputView = require('./views/input_view.js');
+const OutputView = require('./views/output_view.js');
+const WordCounter = require('./models/word_counter.js');
+
+document.addEventListener('DOMContentLoaded', () => {
 console.log('js loaded');
 
+const inputView = new InputView();
+  inputView.bindEvents();
 
-
-);
+});
